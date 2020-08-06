7392560f9d493eb13dfed46086768bfe312a87fa
Jesus Almaral <almaral17@gmail.com>
2020-08-06 17:47:45 -0500

    Create repository to fetch earthquakes

--- app/src/main/java/com/hackaprende/earthquakemonitor/main/MainViewModel.kt
+++ app/src/main/java/com/hackaprende/earthquakemonitor/main/MainViewModel.kt
@@ -5,18 +5,14 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.hackaprende.earthquakemonitor.Earthquake
-import com.hackaprende.earthquakemonitor.api.EarthquakesApi
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import org.json.JSONException
-import org.json.JSONObject
-import java.net.UnknownHostException
 
 class MainViewModel: ViewModel() {
     private val _eqList = MutableLiveData<MutableList<Earthquake>>()
     val eqList: LiveData<MutableList<Earthquake>>
         get() = _eqList
+
+    private val repository = MainRepository()
 
     init {
         getEqList()
@@ -24,49 +20,7 @@ class MainViewModel: ViewModel() {
 
     private fun getEqList() {
         viewModelScope.launch {
-            _eqList.value = fetchEqList()
+            _eqList.value = repository.fetchEqList()
         }
-    }
-
-    private suspend fun fetchEqList(): MutableList<Earthquake> {
-        return withContext(Dispatchers.IO) {
-            val eqList: MutableList<Earthquake> =
-            try {
-                val lastHourEarthquakes = EarthquakesApi.retrofitService.getLastHourEarthquakes()
-                parseStringResult(lastHourEarthquakes)
-            } catch (e: UnknownHostException) {
-                mutableListOf()
-            } catch (e: JSONException) {
-                mutableListOf()
-            }
-            eqList
-        }
-    }
-
-    private fun parseStringResult(stringResult: String): MutableList<Earthquake> {
-        val eqList = mutableListOf<Earthquake>()
-
-        val eqJsonObject = JSONObject(stringResult)
-        val featuresJsonArray = eqJsonObject.getJSONArray("features")
-
-        for (i in 0 until featuresJsonArray.length()) {
-            val jsonFeature = featuresJsonArray[i] as JSONObject
-
-            val id = jsonFeature.getString("id")
-            val jsonProperties = jsonFeature.getJSONObject("properties")
-            val place = jsonProperties.getString("place")
-            val magnitude = jsonProperties.getDouble("mag")
-            val time = jsonProperties.getLong("time")
-            val alert = jsonProperties.getString("alert")
-
-            val jsonCoordinates = jsonFeature.getJSONObject("geometry").getJSONArray("coordinates")
-            val latitude = jsonCoordinates.getDouble(0)
-            val longitude = jsonCoordinates.getDouble(1)
-
-            val earthquake = Earthquake(id, place, magnitude, time, alert, latitude, longitude)
-            eqList.add(earthquake)
-        }
-
-        return eqList
     }
 }
