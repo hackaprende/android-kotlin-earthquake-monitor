5bc5b9abd5b4b4bda5746d109575bd7e705eaa64
Jesus Almaral <almaral17@gmail.com>
2020-08-06 15:59:42 -0500

    Add coroutines to get eqList

--- app/src/main/java/com/hackaprende/earthquakemonitor/main/MainViewModel.kt
+++ app/src/main/java/com/hackaprende/earthquakemonitor/main/MainViewModel.kt
@@ -3,7 +3,11 @@ package com.hackaprende.earthquakemonitor.main
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.hackaprende.earthquakemonitor.Earthquake
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainViewModel: ViewModel() {
     private val _eqList = MutableLiveData<MutableList<Earthquake>>()
@@ -11,23 +15,32 @@ class MainViewModel: ViewModel() {
         get() = _eqList
 
     init {
-        downloadEqList()
+        getEqList()
     }
 
-    private fun downloadEqList() {
-        val eqList = mutableListOf<Earthquake>()
-        eqList.add(Earthquake("1", "Earthquake 1", 5.5, 1234456778L,
-            "red", 24.1029, -110.17236))
-        eqList.add(Earthquake("2", "Earthquake 2", 8.2, 1234456778L,
-            "red", 24.1029, -110.17236))
-        eqList.add(Earthquake("3", "Earthquake 3", 3.0, 1234456778L,
-            "red", 24.1029, -110.17236))
-        eqList.add(Earthquake("4", "Earthquake 4", 4.3, 1234456778L,
-            "red", 24.1029, -110.17236))
-        eqList.add(Earthquake("5", "Earthquake 5", 4.8, 1234456778L,
-            "red", 24.1029, -110.17236))
-        eqList.add(Earthquake("6", "Earthquake 6", 7.1, 1234456778L,
-            "red", 24.1029, -110.17236))
-        _eqList.value = eqList
+    private fun getEqList() {
+        viewModelScope.launch {
+            _eqList.value = fetchEqList()
+        }
+    }
+
+    private suspend fun fetchEqList(): MutableList<Earthquake> {
+        return withContext(Dispatchers.IO) {
+            val eqList = mutableListOf<Earthquake>()
+            eqList.add(Earthquake("1", "Earthquake 1", 5.5, 1234456778L,
+                "red", 24.1029, -110.17236))
+            eqList.add(Earthquake("2", "Earthquake 2", 8.2, 1234456778L,
+                "red", 24.1029, -110.17236))
+            eqList.add(Earthquake("3", "Earthquake 3", 3.0, 1234456778L,
+                "red", 24.1029, -110.17236))
+            eqList.add(Earthquake("4", "Earthquake 4", 4.3, 1234456778L,
+                "red", 24.1029, -110.17236))
+            eqList.add(Earthquake("5", "Earthquake 5", 4.8, 1234456778L,
+                "red", 24.1029, -110.17236))
+            eqList.add(Earthquake("6", "Earthquake 6", 7.1, 1234456778L,
+                "red", 24.1029, -110.17236))
+
+            eqList
+        }
     }
 }
